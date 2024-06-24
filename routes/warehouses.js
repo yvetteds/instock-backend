ccb679f0d95b9fb57c524b3f57325172f0b39098
Yvette de Sereville <ydesereville@uwaterloo.ca>
2024-06-24 16:34:09 -0400

    added .env.sample file for reference for bulding local .env file

---
+++ routes/warehouses.js
@@ -0,0 +1,7 @@
+import express from "express";
+import fs from "fs";
+import uniqid from "uniqid";
+
+const router = express.Router();
+
+// this is where the API endpoints will be built for the warehouses
