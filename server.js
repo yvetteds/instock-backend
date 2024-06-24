d582c7a6a2336fc233445f284e1e75b433e29463
Yvette de Sereville <ydesereville@uwaterloo.ca>
2024-06-24 16:30:11 -0400

    initial commit with initial backend folder structure

---
+++ server.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import warehouses from "./routes/warehouses.js";
+import inventory from "./routes/inventory.js";
+
+const app = express();
+const PORT = process.env.PORT ?? 8080;
+
+app.use(cors());
+app.use(express.json());
+// app.use(express.static("public")); // use if needed for static assets in public folder, don't think will be needed but here just in case
+
+app.use("/warehouses", warehouses);
+app.use("/warehouses/:id", warehouses);
+app.use("/inventory", inventory);
+app.use("/inventory/:id", inventory);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
