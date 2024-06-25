cc3f11b7060749d8aea25024b48ba2c30bdcf47c
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-25 19:49:40 -0300

    create new branch off develop and create API request to get all warehouses

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -8,6 +8,20 @@ const knex = initKnex(configuration);
 const router = express.Router();
 
 // this is where the API endpoints will be built for the warehouses
+/* -------------------------------------------------------------------------- */
+/*                             GET ALL WAREHOUSES                             */
+/* -------------------------------------------------------------------------- */
+router.get("/", async (req, res) => {
+  try {
+    const warehouses = await knex("inventories");
+
+    if (!warehouses) return [];
+
+    res.json(warehouses)
+  } catch (error) {
+    return res.status(500).json("We're unable to your fetch request. Please try again.")
+  }
+})
 
 // GET /warehouses/:id/inventories - getting all inventory items that belog to a warehouse
 router.get("/:id/inventories", async (req, res) => {
