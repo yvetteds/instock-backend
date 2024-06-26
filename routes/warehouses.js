8e8f969d2a7668eb9a2b35e41c7017f8e3d85fbf
jfitt149 <jfitt149@gmail.com>
2024-06-26 11:11:55 -0300

    added res for successful post

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 // this is where the API endpoints will be built for the warehouses
 
-// GET /warehouses/:id/inventories - getting all inventory items that belog to a warehouse
+// GET /api/warehouses/:id/inventories - getting all inventory items that belog to a warehouse
 router.get("/:id/inventories", async (req, res) => {
   try {
     const warehouseInventory = await knex("inventories")
@@ -24,9 +24,57 @@ router.get("/:id/inventories", async (req, res) => {
       });
     }
 
-    res.status(200).json(warehouseInventory);
+    res.json(warehouseInventory);
   } catch (error) {
     res.status(500).send(`Error retrieving warehouse inventory ${error}`);
+  }
+});
+
+// POST /api/warehouses - post a new warehouse
+router.post("/", async (req, res) => {
+  const {
+    warehouse_name,
+    address,
+    city,
+    country,
+    contact_name,
+    contact_position,
+    contact_phone,
+    contact_email,
+  } = req.body;
+
+  if (
+    !warehouse_name ||
+    !address ||
+    !city ||
+    !country ||
+    !contact_name ||
+    !contact_position ||
+    !contact_phone ||
+    !contact_email
+  ) {
+    return res.status(400).json({
+      message: "Missing one or more input fields",
+    });
+  }
+
+  try {
+    const result = await knex("warehouses").insert({
+      warehouse_name: warehouse_name.toUpperCase(),
+      address,
+      city: city.toUpperCase(),
+      country,
+      contact_name: contact_name.toUpperCase(),
+      contact_position: contact_position.toUpperCase(),
+      contact_phone,
+      contact_email,
+    });
+
+    const newWarehouse = { ...result, id: uniqid() };
+
+    res.json(newWarehouse);
+  } catch (error) {
+    res.status(500).send(`Error posting warehouse ${error}`);
   }
 });
 
