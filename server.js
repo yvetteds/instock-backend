a87f832a92dbc7a4665d057b88db409f05a5c404
Yvette de Sereville <ydesereville@uwaterloo.ca>
2024-06-29 10:22:10 -0400

    bug fix on routing in server.js

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json());
 
 app.use("/api/warehouses", warehouses);
-app.use("/api/warehouses/:id", warehouses);
 app.use("/api/inventories", inventory);
-app.use("/api/inventories/:id", inventory);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
